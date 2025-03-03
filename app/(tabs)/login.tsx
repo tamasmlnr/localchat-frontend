1141565a06a20aa13ec79efb249ba7b49b8c8b4d
tamasmlnr <molnart90@gmail.com>
2025-03-03 10:47:08 +0100

    feat: cleanup default project

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from '@/components/ThemedText';
+import React from 'react';
+
+const login = () => {
+    return (
+        <ThemedText>login</ThemedText>
+    );
+};
+
+export default login;
---
+++ theme/theme.tsx
@@ -0,0 +1,12 @@
+
+import { MD3DarkTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+
+export const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#6200ee',
+        secondary: 'yellow',
+        gray: "#666"
+    },
+};
